14b5dd15df0d41c1494a874db09ba4467d269298
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-09-11 08:22:47 -0300

    correcciones del read

--- controllers/activities/read.js
+++ controllers/activities/read.js
@@ -1,20 +1,39 @@
 import Activity from "../../models/Activity.js";
 
-export default async (req,res) => {
+export default async (req,res,next) => {
     
     try {
-        let allActivities = await Activity.find()
+        let objectSearch = {}
        
-        return res.status(200).json({
-            success: true,
-            message: 'activities found',
-            response: allActivities
-        })
+
+        if (req.query.itinerary_id) {
+            objectSearch.itinerary_id = req.query.itinerary_id
+            //new RegExp(req.query.title,'i')
+        }
+        
+
+        let allActivities = await Activity.find()
+        .find(objectSearch,'name  photo itinerary_id ')
+        .populate('itinerary_id','name photo')
+       
+       
+        
+        if (allActivities.length>0) {
+            return res.status(200).json({
+                success: true,
+                message: 'activities found',
+                response: allActivities
+            })}else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'not found',
+                    response: null
+                })
+            } 
     } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: 'not found',
-            response: null
-        })
+        next(error)
     }
 }
+
+   
+   
